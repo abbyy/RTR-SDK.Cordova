991f94cf425af25c6b546483e9fed3364a999bc6
Alexander Lobastov <alexander_l@abbyy.com>
2019-06-20 13:20:36 +0300

    iOS. copy assets from www hook

--- cordova-plugin-abbyy-rtr-sdk/scripts/ios_copy_frameworks.js
+++ cordova-plugin-abbyy-rtr-sdk/scripts/ios_copy_frameworks.js
@@ -9,7 +9,8 @@ const xcodeprojPath = findInDirectory('platforms/ios', '.xcodeproj');
 const pbxprojPath = xcodeprojPath + '/project.pbxproj';
 const pbxproj = xcode.project(pbxprojPath);
 
-var options = { shellPath: '/bin/sh', shellScript: '/bin/sh "../../libs/ios/copy_frameworks.sh"' };
+var options_libs = { shellPath: '/bin/sh', shellScript: '/bin/sh "../../libs/ios/copy_frameworks.sh"' };
+var options_assets = { shellPath: '/bin/sh', shellScript: '[ -d "$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME/www/rtr_assets" ] && rsync -Lrav "$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME/www/rtr_assets/" "$TARGET_BUILD_DIR/$WRAPPER_NAME"' };
 
 var frameworksPath = {
 	path:'../../libs/ios',
@@ -18,7 +19,8 @@ var frameworksPath = {
 }
 
 pbxproj.parse(function(err) {
-	pbxproj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Copy Frameworks', pbxproj.getFirstTarget().uuid, options);
+	pbxproj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Copy Frameworks', pbxproj.getFirstTarget().uuid, options_libs);
+	pbxproj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Copy Assets', pbxproj.getFirstTarget().uuid, options_assets);
 	pbxproj.addToFrameworkSearchPaths(frameworksPath);
 	fs.writeFileSync(pbxprojPath, pbxproj.writeSync());
 })
